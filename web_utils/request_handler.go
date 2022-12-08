12e1064ef8ff5a0a7d09ac147d1940a46f216c31
dyhabo <dylanhb@live.co.uk>
2022-12-08 21:35:18 +0000

    Verify TLS cert

--- web_utils/request_handler.go
+++ web_utils/request_handler.go
@@ -1,9 +1,11 @@
 package web_utils
 
 import (
+	"crypto/tls"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type requestHandler struct {
@@ -18,12 +20,35 @@ func newRequestHandler(saveDir string, baseUrl string) requestHandler {
 	}
 }
 
-func (r requestHandler) downloadFile(filename string, path string) (status int, err error) {
+// isTLSCertValid checks for a TLS certification against a domain.
+func isTLSCertValid(url string) (bool, error) {
+	url = getDomain(url)
+	conn, err := tls.Dial("tcp", url, nil)
+	if err != nil {
+		return false, err
+	}
+	err = conn.VerifyHostname(url)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func getDomain(url string) string {
+	strings.TrimPrefix(url, "https://")
+	strings.TrimPrefix(url, "http://")
+	strings.TrimPrefix(url, "www.")
+	i := strings.Index(url, "/")
+	return url[0:i]
+}
+
+// downloadFile makes a GET request to baseUrl + path.
+func (r requestHandler) downloadFile(filename string, path string) (statusCode int, err error) {
 	if r.saveDir == "" || r.baseUrl == "" {
 		return 0, nil
 	}
 
-	out, err := os.Create(r.baseUrl + "/" + filename)
+	out, err := os.Create(r.saveDir + "/" + filename)
 	if err != nil {
 		return 0, err
 	}
